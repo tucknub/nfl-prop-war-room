5be8761026a0bc226952937f32fb1022a9d39fc6
tucknub <126122166+tucknub@users.noreply.github.com>
2026-08-28 19:37:06 -0400

    refactor: remove obsolete Today weekly wrappers

--- dashboard/propwar_today_owner.py
+++ dashboard/propwar_today_owner.py
@@ -173,22 +173,6 @@ def _today_trending_adds():
         )
 
 
-@st.cache_data(ttl=60, show_spinner=False, refresh_mode="background")
-def _today_matchups(league_id: str, week: int):
-    if week < 1:
-        return ()
-    with SleeperClient() as client:
-        return client.fetch_matchups(league_id, week)
-
-
-@st.cache_data(ttl=60, show_spinner=False, refresh_mode="background")
-def _today_transactions(league_id: str, week: int):
-    if week < 1:
-        return ()
-    with SleeperClient() as client:
-        return client.fetch_transactions(league_id, week)
-
-
 @st.cache_data(ttl=5 * 60, show_spinner=False, refresh_mode="background")
 def _today_fantasy_feed(
     user_id: str,
